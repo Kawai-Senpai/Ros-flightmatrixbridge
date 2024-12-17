890e2a7dff7d6a86d274f6b2e9227ad6503d9722
Ranit Bhowmick <104001870+Kawai-Senpai@users.noreply.github.com>
2024-12-17 23:02:49 +0530

    Update setup.py to specify version and dependencies; add empty resource directory
    Problem
    =======
    There were some mistakes in the previous commits
    
    Solution
    ========
    Fixed mistakes in the previous commits
    
    Note
    ====
    -

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import setup
 
-# Read requirements from requirements.txt
-def get_requirements():
-    with open('requirements.txt') as f:
-        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
-
 package_name = 'flightmatrix_ros2'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'] + get_requirements(),
+    install_requires=[
+        'setuptools',
+        'flightmatrixbridge==1.7.0',
+        'transforms3d==0.4.2'
+    ],
     zip_safe=True,
     maintainer='Ranit Bhowmick',
     maintainer_email='bhowmickranitking@duck.com',
